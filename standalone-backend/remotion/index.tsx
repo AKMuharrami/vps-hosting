88ec295bbe6f5aea5b98dfc32a6e4cab09e6b2fa
Asad Al Muharrami <akmuharrami@gmail.com>
2026-05-02 23:12:32 +0400

    AAA

--- standalone-backend/remotion/index.tsx
+++ standalone-backend/remotion/index.tsx
@@ -1,26 +1,23 @@
-import { registerRoot } from 'remotion';
-import React from 'react';
-import { Composition } from 'remotion';
+import { registerRoot, Composition } from 'remotion';
 import { CaptionsComposition } from './CaptionsComposition';
 
-export const RemotionRoot: React.FC = () => {
-    return (
-        <>
-            <Composition
-                id="Captions"
-                component={CaptionsComposition}
-                durationInFrames={300}
-                fps={30}
-                width={1080}
-                height={1920}
-                defaultProps={{
-                    videoUrl: '',
-                    captions: [],
-                    styleOptions: {}
-                }}
-            />
-        </>
-    );
+export const RemotionRoot = () => {
+  return (
+    <Composition
+      id="Captions"
+      component={CaptionsComposition}
+      durationInFrames={300} // Will be overridden dynamically
+      fps={30}
+      width={1080}
+      height={1920}
+      defaultProps={{
+        videoUrl: '',
+        captions: [],
+        styleOptions: {},
+        videoHeight: 1920
+      }}
+    />
+  );
 };
 
 registerRoot(RemotionRoot);
